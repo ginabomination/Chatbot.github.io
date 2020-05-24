2c91a66485e96c5837f0e45ff51a872bd2ca8b68
ginabomination <64622072+ginabomination@users.noreply.github.com>
2020-05-25 11:32:42 +1200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,37 +1,46 @@
-var userInput = document.querySelector("#userInput");
-var submit = document.querySelector("#submit");
-var botChat = document.querySelector("#botChat");
-var user = document.querySelector("#user");
+const userInput = document.querySelector("#userInput");
+const submit = document.querySelector("#submit");
+const botChat = document.querySelector("#botChat");
+const user = document.querySelector("#user");
 
-alert ("testing")
+alert("Dear user, please refrain from using capital letters. To answer a question use y or n. Thank you")
 
 submit.addEventListener("click", response);
 
 greetingList = ["Hello", "Hi", "Hey there", "Yo"];
-
 greeting = Math.floor(Math.random() * greetingList.length);
 
-function response() {
-    while(botChat.textContent == "What is your name?") {
-        user.style.color = "red";
-        botChat.innerHTML = greetingList[greeting] + " " + userInput.value;
-        user.innerHTML = userInput.value;
-        setTimeout(() => {botChat.innerHTML = "What is your favourite colour?" }, 2000);;
-        userInput.value = null;
+function response()
+const red = user.style.colour ("#DC143C")
+const orange = user.style.colour ("#FF8C00")
+const yellow = user.style.colour ("#FFD700")
+const green = user.style.colour ("#7FFF00")
+const blue = user.style.colour ("#228B22")
+const purple = user.style.colour ("#8B008B") 
+{
+    (botChat.textContent == "What is your name?") {
+    botChat.innerHTML = greetingList[greeting] + " " + userInput.value;
+    setTimeout(() => {botChat.innerHTML = "What is your favourite color?" }, 2000);;
+    userInput.value = null;
     } 
-    while (botChat.extContent == "What is your name?") {
-        setTimeout(() => {botChat.innerHtml = "What is your favourite colour?
-        userInput.value = user.style.colour
-        if (userInput.value == "green", "Green") {
-            botChat.innerHtml = "Mine too")
+    while(botChat.textContent == "What is your favourite color?"){
+       if (userInput.value == "green", "Green") {
+       	botChat.innerHTML = "Mine too";
+	user.style.colour = green;
+	userInput.value = null;
+       }
+    if (userInput.value == "red", "Red") {
+        botChat.innerHTML = "Pity, mines green.";
+	user.style.colour = red;
+	userInput.value = null;
+    }
         }
-        else if (userInupt.value == "red", "Red", "orange", "Orange", "yellow", "Yellow", "green", "Green", "blue", "Blue", "purple", "Purple") {                          
-            userInput.value = user.style.colour 
+        else if (userInput.value == , "orange", "Orange", "yellow", "Yellow", "blue", "Blue", "purple", "Purple") {
+            botChat.textContent = "Pity, mines green.";
         }
         else {
-            botChat.textContent = "Please enter a simple colour. Im not an expert in my colours.";
-            setTimeout (() => {botChat.innerHtml = "What is your favourite colour?"}, 2000;;
-            userInput.value = null;
+            botChat.textContent = "Please type a basic colour.";
+            setTimeout(() => {botChat.innerHTML = "What is your favourite color?" }, 2000);;
         }
     }
 }
