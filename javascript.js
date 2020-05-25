4a0631f30887f9711d1e508421f36f80923a777c
ginabomination <grugerstorz@gmail.com>
2020-05-26 09:16:31 +1200

    fixing errors and colour change

--- javascript.js
+++ javascript.js
@@ -2,29 +2,37 @@ const userInput = document.querySelector("#userInput");
 const submit = document.querySelector("#submit");
 const botChat = document.querySelector("#botChat");
 const user = document.querySelector("#user");
+const colourrange = ["red", "yellow", "orange", "blue", "purple", "Red", "Yellow", "Orange", "Blue", "Purple"]
 
 alert("Dear user, please refrain from using capital letters. To answer a question use y or n. Thank you")
 
 submit.addEventListener("click", response);
 
 const greetingList = ["Hello", "Hi", "Hey there", "Yo"];
-greeting = Math.floor(Math.random() * greetingList.length); 
+const greeting = Math.floor(Math.random() * greetingList.length); 
 
 function response(){
     (botChat.textContent == "What is your name?") 
     botChat.innerHTML = greetingList[greeting] + " " + userInput.value;
+    user.textContent = userInput;
     setTimeout(() => {botChat.innerHTML = "What is your favourite color?" }, 2000);;
     userInput.value = null;
     submit.addEventListener("click", colour);
     } 
-
-    function colour () {
-
-    while(botChat.textContent == "What is your favourite color?"){
+const green = style.colour ("green")
+const red = style.colour ("red")
+function colour () {
+    (botChat.textContent == "What is your favourite color?");
        if (userInput.value == "green", "Green") {
        	botChat.textContent = "Mine too";
-	user.style.colour = green;
-	userInput.value = null;
+	    user.style.colour = "green";
+	    userInput.value = null;
         }
-    }
-
+        else (userInput.value == colourrange) ;{
+            botChat.textContent = "Pity, mines green.";
+            user.style.colour = "userInput.value";
+            userInput.value = null;
+        }
+        
+}
+    
