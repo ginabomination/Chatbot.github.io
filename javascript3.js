f0f510588c9ddcaaed776f21630c1e12635460d7
ginabomination <grugerstorz@gmail.com>
2020-06-12 20:40:32 +1200

    finale version

--- javascript3.js
+++ javascript3.js
@@ -4,9 +4,6 @@ const botChat = document.querySelector("#botChat");
 const user = document.querySelector("#user");
 const beyond = document.querySelector("#beyond");
 const hideandshow = document.querySelector("#showelement");
-const roll = document.querySelector("#roll");
-const rollNo = document.querySelector("#rollNo");
-const userRoll = document.querySelector("#userRoll");
 
 const greetingList = ["Hello", "Hi", "Hey there", "Yo"];
 const greeting = Math.floor(Math.random() * greetingList.length); // array for hellos
@@ -16,26 +13,6 @@ alert("Dear user, please refrain from using capital letters. To answer a questio
 submit.addEventListener("click", response); // submit button
 beyond.addEventListener("click", Beyond); // button to open link to D&D Beyond
 hideandshow.addEventListener("click", showElement); // button to hide {Name}
-roll.addEventListener ("click", Roll)
-
-const d10 = ["1","2","3","4","5","6","7","8","9","10"];
-const randomd10 = d10[Math.floor(Math.random() * d10.length)];
-const d8 = ["1","2","3","4","5","6","7","8"];
-const randomd8 = d8[Math.floor(Math.random() * d8.length)];
-
-function Roll () {
-    while(roll.textContent == "Roll") {
-        if (userRoll.value == "1d10", "1D10") {
-        rollNo.innerHTML = randomd10[d10]; 
-        setTimeout(() => {rollNo.innerHTML = "Dice roller" }, 2000);;}// if user rolls a 4 sided die with a +1 modifer [1d4+1 is a term used in common rpg play]
-
-    if (userRoll.value == "1d8", "1D8") {
-        rollNo.innerHTML = randomd8[d8]; 
-        setTimeout(() => {rollNo.innerHTML = "Dice roller" }, 2000);;}
-
-        else {
-            botChat.textContent = "did not calculate. try again.";
-        setTimeout(() => {rollNo.innerHTML = "Dice roller" }, 2000);;}}}
 
     function showElement (){
         if (user.style.display === "none") {
@@ -95,4 +72,15 @@ function response() {
                 setTimeout(() => {botChat.innerHTML = "Did you know warlocks have wisdom and charisma saving throws?" }, 2000);;
 
         userInput.value = null;}
-        }}
+        
+        setTimeout(() => {botChat.innerHTML = "Do you know any common patrons?" }, 2000);; // next question
+        userInput.value = null;} // cleans the user input
+        
+        while(botChat.textContent == "Do you know any common patrons?") {
+            if (userInput.value == "yes") {
+                botChat.innerHTML = "I would love to have tenticles if I had a patrom. Thank you for using my Chatbot.";
+                userInput.value = null;}
+            
+                else if (userInput.value == "no") {
+                botChat.textContent = "I would love to have the lurker of the dept to be my patreon, I would love to have tenticles. Thank you for using my Chatbot.";
+                userInput.value = null;}}}
