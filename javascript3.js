df2eed00af4ad7c8c591f0612e81d311e5cc7ec6
ginabomination <grugerstorz@gmail.com>
2022-10-18 12:17:58 +1300

    layout simplification

--- javascript3.js
+++ javascript3.js
@@ -2,8 +2,6 @@ const userInput = document.querySelector("#userInput");
 var submit = document.querySelector("#submit");
 const botChat = document.querySelector("#botChat");
 const user = document.querySelector("#user");
-const beyond = document.querySelector("#beyond");
-const hideandshow = document.querySelector("#showelement");
 
 const greetingList = ["Hello", "Hi", "Hey there", "Yo"];
 const greeting = Math.floor(Math.random() * greetingList.length); // array for hellos
@@ -11,19 +9,7 @@ const greeting = Math.floor(Math.random() * greetingList.length); // array for h
 alert("Dear user, please refrain from using capital letters. To answer a question use y or n. Scroll up and down the page to change background. Thank you") // instructions for use
 
 submit.addEventListener("click", response); // submit button
-beyond.addEventListener("click", Beyond); // button to open link to D&D Beyond
-hideandshow.addEventListener("click", showElement); // button to hide {Name}
 
-    function showElement (){
-        if (user.style.display === "none") {
-            user.style.display = "block";
-        } else {
-            user.style.display = "none";
-        }
-      } // button to hide {Name}, partcially sourced from w3schools
-
-function Beyond (){
-window.open("https://www.dndbeyond.com/classes/warlock")} // button to open link to D&D Beyond [my own work]
 
 window.onload = function question () {
     botChat.textContent = "What is your name?" // means that this question is the first question to show when the page is opened
